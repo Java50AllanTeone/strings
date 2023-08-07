ddbc0f446cec5468bdd21c15ceb35e841bec5faf
allanteone <allanteone@gmail.com>
2023-08-07 17:00:19 +0300

    implements tests

--- strings/src/telran/QuickDictionary.java
+++ strings/src/telran/QuickDictionary.java
@@ -3,43 +3,43 @@ package telran;
 import java.util.HashMap;
 import java.util.Map;
 
-class QuickDictionary {
+public class QuickDictionary {
 	private Node rootNode = new Node();
 
-	String put(String key, String value) {
-    	var chars = key.toCharArray();
-    	var curNode = rootNode;
-    	
-    	for (char ch : chars) {
-    		curNode.lookup.putIfAbsent(ch, new Node());
-    		curNode = curNode.lookup.get(ch);
-    	}
-    	
-    	String prev = curNode.dictionaryVal;
-    	curNode.dictionaryVal = value;
-    	return prev;
-    }
+	public String put(String key, String value) {
+		var chars = key.toCharArray();
+		var curNode = rootNode;
 
-    String get(String key) {
-    	var chars = key.toCharArray();
-    	var curNode = rootNode;
-    	
-    	for (char ch : chars) {
-    		if (curNode.lookup.containsKey(ch)) {
-    			curNode = curNode.lookup.get(ch);
-    		} else {
-    			return null;
-    		}
-    	}
-    	
-    	return curNode.dictionaryVal;
-    }
-    
-    
-    
-    
-    private class Node {
-    	Map<Character, Node> lookup = new HashMap<>();
-    	String dictionaryVal;
-    }
+		for (char ch : chars) {
+			if (ch < 65 || ch > 90) {
+				throw new IllegalArgumentException();
+			}
+			curNode.lookup.putIfAbsent(ch, new Node());
+			curNode = curNode.lookup.get(ch);
+		}
+		String prev = curNode.dictionaryVal;
+		curNode.dictionaryVal = value;
+		return prev;
+	}
+
+	public String get(String key) {
+		var chars = key.toCharArray();
+		var curNode = rootNode;
+
+		for (char ch : chars) {
+			if (ch < 65 || ch > 90) {
+				throw new IllegalArgumentException();
+			} else if (curNode.lookup.containsKey(ch)) {
+				curNode = curNode.lookup.get(ch);
+			} else {
+				return null;
+			}
+		}
+		return curNode.dictionaryVal;
+	}
+
+	private class Node {
+		Map<Character, Node> lookup = new HashMap<>();
+		String dictionaryVal;
+	}
 }
